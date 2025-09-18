5d4efcac75e04863e45687a46ebe554210e4e535
srandres-git <andysanchez2001@gmail.com>
2025-09-18 12:40:52 -0600

    Implement file upload state functionality and reconciliation button

---
+++ conc.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+
+def conciliar(sat: pd.DataFrame, sap: pd.DataFrame, box: pd.DataFrame, cp: pd.DataFrame):
+    st.write('Conciliado')
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 from config import TAB_NAMES
+from conc import conciliar
 
 st.title("Conciliación de facturas recibidas")
 
@@ -12,6 +13,7 @@ tab_dict = {tab_name:tab for tab_name,tab in zip(TAB_NAMES,tabs)}
 with tab_dict['Generar conciliación']:
     cols = st.columns(4)
 
+    files_status = {file:False for file in ['sat','sap','box','cp']}
     # agregamos los file uploaders
     file_fact_sat = cols[0].file_uploader('Facturas recibidas SAT',type='xlsx',accept_multiple_files=False)
     file_fact_sap = cols[1].file_uploader('Facturas SAP',type='xlsx',accept_multiple_files=False)
@@ -20,17 +22,31 @@ with tab_dict['Generar conciliación']:
 
     # leemos los reportes
     if file_fact_sat:
+        files_status['sat'] = False
         fact_sat = pd.read_excel(file_fact_sat, header=4)
+        files_status['sat'] = True
         cols[0].write('Facturas leídas correctamente.')
 
     if file_fact_sap:
+        files_status['sap'] = False
         fact_sap = pd.read_excel(file_fact_sap, header=9)
+        files_status['sap'] = True
         cols[1].write('Facturas leídas correctamente.')
     
     if file_box:
+        files_status['box'] = False
         box = pd.read_excel(file_box, header=0)
+        files_status['box'] = True
         cols[2].write('Reporte de Box leído correctamente.')
 
     if file_cp:
+        files_status['cp'] = False
         cp = pd.read_excel(file_cp, header=4)
+        files_status['cp'] = True
         cols[3].write('Complementos de pago leídos correctamente.')
+
+    st.session_state['conc_button'] = st.container(key='conc_button')
+    st.session_state['download_button'] = st.container(key='download_button')
+    if sum(files_status.values())==4:
+        with st.session_state['conc_button']:
+            conciliacion = st.button('Conciliar', on_click=conciliar, args=[fact_sat,fact_sap,box,cp])
