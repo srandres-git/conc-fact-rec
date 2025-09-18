15ea0bc03e36c96b43472ff167d01994b064b597
srandres-git <andysanchez2001@gmail.com>
2025-09-18 12:13:26 -0600

    Eliminate update message placeholders

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,34 +14,23 @@ with tab_dict['Generar conciliación']:
 
     # agregamos los file uploaders
     file_fact_sat = cols[0].file_uploader('Facturas recibidas SAT',type='xlsx',accept_multiple_files=False)
-    placeholder_sat = cols[0].empty().container() # container del mensaje de actualización
-
     file_fact_sap = cols[1].file_uploader('Facturas SAP',type='xlsx',accept_multiple_files=False)
-    placeholder_sap = cols[1].empty().container()
-
     file_box = cols[2].file_uploader('Box', type='xlsx', accept_multiple_files=False)
-    placeholder_box = cols[2].empty().container()
-
     file_cp = cols[3].file_uploader('Complementos de pago', type='xlsx', accept_multiple_files=False)
-    placeholder_cp = cols[3].empty().container()
 
     # leemos los reportes
     if file_fact_sat:
-        placeholder_sat.write('Leyendo reporte de facturas SAT...')
         fact_sat = pd.read_excel(file_fact_sat, header=4)
-        placeholder_sat.write('Facturas leídas correctamente.')
+        cols[0].write('Facturas leídas correctamente.')
 
     if file_fact_sap:
-        placeholder_sap.write('Leyendo saldos de facturas SAP...')
         fact_sap = pd.read_excel(file_fact_sap, header=9)
-        placeholder_sap.write('Facturas leídas correctamente.')
+        cols[1].write('Facturas leídas correctamente.')
     
     if file_box:
-        placeholder_box.write('Leyendo reporte de Box...')
         box = pd.read_excel(file_box, header=0)
-        placeholder_box.write('Reporte de Box leído correctamente.')
+        cols[2].write('Reporte de Box leído correctamente.')
 
     if file_cp:
-        placeholder_cp.write('Leyendo complementos de pago...')
         cp = pd.read_excel(file_cp, header=4)
-        placeholder_cp('Complementos de pago leídos correctamente.')
+        cols[3].write('Complementos de pago leídos correctamente.')
