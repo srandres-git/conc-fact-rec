088d94364164dd440c5667e1c163c2d765a5e92a
srandres-git <andysanchez2001@gmail.com>
2025-09-18 11:32:07 -0600

    Refactor initial devcontainer configuration and update requirements; enhance streamlit app for invoice reconciliation

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
+import pandas as pd
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Conciliación de facturas recibidas")
+file_fact_sat = st.file_uploader('Facturas recibidas SAT',type='xlsx',accept_multiple_files=False)
+
+if file_fact_sat:
+    st.write('Leyendo reporte de facturas SAT...')
+    fact_sat = pd.read_excel(file_fact_sat, header=4)
+    st.write('Facturas leídas correctamente.')
