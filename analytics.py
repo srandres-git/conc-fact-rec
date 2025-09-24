85b37601b6e74669c4722f5714948df639d4f1b2
srandres-git <andysanchez2001@gmail.com>
2025-09-24 13:13:39 -0600

    Refactor format_func in create_dashboard function for improved readability and structure

--- analytics.py
+++ analytics.py
@@ -18,5 +18,9 @@ def create_dashboard(conciliacion: pd.DataFrame):
         values={'Total SAT MXN':'sum','Total SAP MXN':'sum', 'Dif. Total MXN':'sum', 'UUID':'count'},
         filters={'Mes':None, 'Estatus Box':None, 'Ejecutivo CxP':None, 'Tipo de servicio':SERVS_TRANSPORTE},
         container=st.container(),
-        format_func= lambda x: f":red[{x}]" if 'Revisar' in x else f":green[{x}]" if 'OK' in x else f"{x:,.2f}" if isinstance(x, float) else x
+        format_func= lambda x: f"{x:,.2f}" if isinstance(x, float) \
+            else f"{x:,}" if isinstance(x, int) \
+            else f":red[{x}]" if 'Revisar' in x and isinstance(x, str) \
+            else f":green[{x}]" if 'OK' in x and isinstance(x, str)\
+            else x
     )
