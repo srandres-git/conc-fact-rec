1eefff817b2f9c4b48d57679dcd73c126e4cfbb4
srandres-git <andysanchez2001@gmail.com>
2025-09-25 12:49:43 -0600

    Eliminate assignment of session state for multiselect widget

--- utils.py
+++ utils.py
@@ -229,16 +229,17 @@ def dynamic_table(
         if preselected:
             preselected = [val for val in preselected if val in unique_vals]# correct preselected to make sure the value exists
         ms_key = f"ms_{name}_{col.replace(' ','_')}"
-        if st.session_state.get('sstate'+ms_key) is None:
-            st.session_state['sstate'+ms_key] = st.multiselect(
+        if st.session_state.get(ms_key) is None:
+            selected = st.multiselect(
                 f"{col}",
                 options=unique_vals,
                 default=preselected,
                 # generate a unique key using the name and column name
                 key=ms_key
             )
-        selected = st.session_state['sstate'+ms_key]
-        st.session_state['sstate'+ms_key]
+        else:
+            selected = st.session_state.get(ms_key) 
+        # display the multiselect with the selected values        
         if selected:
             filtered_df = filtered_df[filtered_df[col].isin(selected)]
 
