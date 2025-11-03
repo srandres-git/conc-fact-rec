Fix pivot table total row calculation: replace append with pd.concat ('append' is depricated)

analytics.py
@@ -184,7 +184,7 @@ def pivot_table(
     if total_row:
         total_series = pivot_df.sum(numeric_only=True)
         total_series.name = 'Total'
-        pivot_df = pivot_df.append(total_series)
+        pivot_df = pd.concat([pivot_df, total_series.to_frame().T])
     # Add total column if specified
     if total_col:
         pivot_df['Total'] = pivot_df.sum(axis=1, numeric_only=True)